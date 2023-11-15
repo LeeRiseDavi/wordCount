409efe367a234096806826823b59817be192c8ff
LeeRiseDavi <150865596+LeeRiseDavi@users.noreply.github.com>
2023-11-14 20:20:28 -0600

    Update main.py
    Added notes

--- main.py
+++ main.py
@@ -2,10 +2,14 @@
 # This program counts the number of words in a line
 # The line being counted comes from the user
 
+# this prompts the user to write something
 print('This program figures out the number of words in a line entered by a user. Please enter text: ')
 
-line = input()
+# this ensures that the response is a string
+line = str(input())
 
+# this splits the words and counts them
 total_count = len(line.split())
 
+# this prints the results
 print('This line contains: ', total_count, 'words.')
